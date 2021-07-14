d1562c2c953e2a6cb50159db23b4f910a8a51a58
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-14 18:41:01 -0400

    Inputs inside script

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
-
+## inputs can be loaded using special varialbles
+##0-n,@,#,*
+# in scriot if you want to accecc scriptname then $0
 echo $0
+# if you want to access the first argument then $1
 echo $1
+# pull all the parsed values
+echo $*
+echo $@
+#number of values parsed is $#
+echo $#
