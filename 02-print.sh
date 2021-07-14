027f152f001e672cff10b27e36fd9e84104be3ca
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-13 21:20:27 -0400

    printing messages and also using color codes

--- 02-print.sh
+++ 02-print.sh
@@ -8,3 +8,16 @@
 # syntax : echo <MESSAGE>
 
 echo HELLO CHANDU
+
+# print multiple lines : for this use \n , see the below example to understand
+
+echo -e "HELLO CHANDU\n\twhatsup?"
+## NOTE : Here in above syntax after echo we used -e which means MESSAGE need to be typed in Quotes
+##        \n is used to give multiple lines , HELLO CHANDU is displayed in one line  and whatsup? is displayed in followed second line
+##        \t is for tab space
+
+ ## syntax for color : echo -e "\e[COLOR_CODEmMESSAGE"
+ # COLORS ARE : RED(31), GREEN(32), YELLOW(33), BLUE(34), MAGNETA(35), CYANT(36
+
+ echo -e "\e[31mCHANDU"
+ ## The above command give CHANDU in RED color
