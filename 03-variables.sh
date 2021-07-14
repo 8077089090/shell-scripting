30b5477b59121e205e96764ec7b0fa11a1447eee
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-14 13:04:35 -0400

    variables practice and understanding

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,5 +5,5 @@ echo B=$A
 ## In the above text, A is a variable as it stores some data in it(Mr.perfect) and to represent/ access that data we need to give "$" sign that why we given echo B=$A
 
 ## command substitutes
-NO_OF_USERS=$(who |wc-l)
+NO_OF_USERS=$(who |wc -l)
 echo number of users=$NO_OF_USERS
