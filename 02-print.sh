ffd42234442c49b5e75a89fee95fcfe6e7842b6d
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-13 20:32:49 -0400

    small automation using bash shell

---
+++ 02-print.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+## printing something on screen can be done by two commands
+## 1. printf
+##2. echo
+
+##  in the above two commands, echo is widely used since it is more descriptive inspite doing all the things that printf does which will have more syntaxes
+# syntax : echo <MESSAGE>
+
+echo HELLO CHANDU
