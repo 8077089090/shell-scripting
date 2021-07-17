afcc5390ab1d6beec90c49a7cf8b146f60021ef9
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-17 18:01:09 -0400

    Roboshop project scripting

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,9 +4,9 @@ rm -f $LOG
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
  yum install nginx -y &>>$LOG
  if [ $? -eq 0 ] ; then
-   echo done
+   echo -e "\e[32mDONE\e[0m"
  else
- echo fail
+ echo -e "\e[31mFAIL\e[0m"
  fi
 echo -e "Enabling Nginx\t\t\t...\t\e[32mdone\e[0m"
  systemctl enable nginx &>>LOG
