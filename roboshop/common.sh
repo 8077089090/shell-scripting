03ceffca6420002d115fa34fb2dc80cafe0a0b92
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-17 19:26:25 -0400

    Roboshop project scripting

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -9,3 +9,25 @@ USERID=$(id -u)
   LOG=/tmp/roboshop.log
   rm -f $LOG
 
+STATUSCHECK() {
+   if [ $1 -eq 0 ] ; then
+   echo -e "\e[32mDONE\e[0m"
+   else
+   echo -e "\e[31mFAIL\e[0m"
+   exit 1
+   fi
+ }
+ STAMP() {
+   echo -n -e "$1\t\t..."
+ }
+STAMP "Installing Nginx"
+ yum install nginx -y &>>$LOG
+STATUSCHECK $?
+
+STAMP "Enabling Nginx\t"
+ systemctl enable nginx &>>LOG
+STATUSCHECK $?
+
+STAMP "starting Nginx\t"
+ systemctl start nginx &>>LOG
+STATUSCHECK $?
--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/bash
-LOG=/tmp/roboshop.log
-rm -f $LOG
 
-STATUSCHECK() {
-   if [ $1 -eq 0 ] ; then
-   echo -e "\e[32mDONE\e[0m"
-   else
-   echo -e "\e[31mFAIL\e[0m"
-   exit 1
-   fi
- }
- STAMP() {
-   echo -n -e "$1\t\t..."
- }
 STAMP "Installing Nginx"
  yum install nginx -y &>>$LOG
 STATUSCHECK $?
