6c2dd86bedb32b9ca2d856e8a30a8a05e95c6318
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-17 20:00:55 -0400

    Roboshop project scripting

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,7 +2,23 @@
 
 source common.sh
 STAMP "Installing Nginx"
- yum install nginx -y &>>$LOG
+yum install nginx -y
+STATUSCHECK $?
+
+STAMP "Download frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STATUSCHECK $?
+
+STAMP "Remove old HTdocs"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+STATUSCHECK $?
+
+STAMP "Extract frontend and archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+STATUSCHECK $?
+
+STAMP "Update roboshop config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STATUSCHECK $?
 
 STAMP "Enabling Nginx\t"
