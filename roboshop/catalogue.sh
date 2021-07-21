1b9fb047f54dc5f397880e708a615d5bad128750
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-21 19:33:19 -0400

    Roboshop project scripting

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -12,16 +12,14 @@ if [ $? -ne 0 ]; then
 fi
 STATUSCHECK $?
 
-So let's switch to the roboshop user and run the following commands.
+STAMP "Download application code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STATUSCHECK $?
 
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
-NOTE: We need to update the IP address of MONGODB Server in systemd.service file. Replace MONGO_DNSNAME with IP address.
-Now, lets set up the service with systemctl.
+STAMP "Extract downloaded code"
+cd /home/roboshop &&  unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+STATUSCHECK $?
+
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
