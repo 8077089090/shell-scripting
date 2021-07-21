0e9779c9d8915399717137cd415d2fd46750565b
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-21 19:07:14 -0400

    Roboshop project scripting

---
+++ roboshop/catalogue.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+source common.sh
+
+STAMP "Install nodejs file"
+yum install nodejs make gcc-c++ -y &>>$LOG
+STATUSCHECK $?
+
+STAMP "Add roboshop appllication user"
+useradd roboshop &>>$LOG
+STATUSCHECK $?
+
+So let's switch to the roboshop user and run the following commands.
+
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+$ cd /home/roboshop
+$ unzip /tmp/catalogue.zip
+$ mv catalogue-main catalogue
+$ cd /home/roboshop/catalogue
+$ npm install
+NOTE: We need to update the IP address of MONGODB Server in systemd.service file. Replace MONGO_DNSNAME with IP address.
+Now, lets set up the service with systemctl.
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
