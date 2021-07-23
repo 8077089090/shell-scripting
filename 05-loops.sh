52cca77368659eaad80b0f6e2b8c6e67f8419284
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-22 20:38:21 -0400

    Roboshop project scripting

---
+++ 05-loops.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+## loops: 1. while 2.for
+# while loop ;
+#while [ expression ]; do
+#commands
+#done
+
+##example for while loop : Run a loop for 10 times
+i=10
+while [ $i -gt 0 ]; do
+  echo iteration=$i
+done
