8dc0ca2be0400723d89e8f5c72610620f95e8aaa
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-22 20:50:05 -0400

    Roboshop project scripting

--- 05-loops.sh
+++ 05-loops.sh
@@ -12,3 +12,13 @@ while [ $i -gt 0 ]; do
   echo iteration=$i
   i=$(($i-1))
 done
+
+# for loop :
+# for var in value1 value2 value3 value4 value5 ...valuen
+# commands
+#done
+
+# example
+for cricketers in dhoni kohli dravid ganguly sachin ; do
+  echo cricketers
+done
