cc4bb12d249f74b8e14273aa2d973ad8a4716085
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-23 19:59:46 -0400

    Roboshop project scripting

--- terraform/02-variables/vars.tf
+++ terraform/02-variables/vars.tf
@@ -11,3 +11,4 @@ output "sample1" {
 output "sample2" {
   value = "${var.sample} - From chandu"
 }
+// in above third case From chandu is a string added to variable , in that case we need to follow "${ } - string " this syntax
