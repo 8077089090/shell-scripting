5b299c62c1d9fc7089a45212b6c92729fce1ddc0
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-23 20:02:41 -0400

    Roboshop project scripting

--- terraform/02-variables/vars.tf
+++ terraform/02-variables/vars.tf
@@ -12,3 +12,8 @@ output "sample2" {
   value = "${var.sample} - From chandu"
 }
 // in above third case From chandu is a string added to variable , in that case we need to follow "${ } - string " this syntax
+// Now in below example, we are not giving any value to variable so in this case terraform asks to enter value after execution
+variable "test" {}
+output "test" {
+  value = var.test
+}
