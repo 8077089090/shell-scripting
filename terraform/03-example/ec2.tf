0fc94b97ab1c2212ca9e652c0de1ea0b793dd71d
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-25 14:37:22 -0400

    Roboshop project scripting

--- terraform/03-example/ec2.tf
+++ terraform/03-example/ec2.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "terraform_instance" {
   ami                    = "ami-074df373d6bafa625"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [ "aws_security_group.allow_ssh.id" ]
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
 
   tags                   = {
    Name                  = "terraform_instance"
