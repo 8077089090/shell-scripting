36333537949e8fb4820d8f4f5857099e60b2fb6f
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-26 17:06:36 -0400

    Roboshop project scripting

---
+++ terraform/04-remotestate/sample.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "terraform_instance" {
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t3.micro"
+  tags                   = {
+    Name                 = "terraform_instance"
+  }
+}
+
+provider "aws" {
+         region= "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-statefile-practice"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
