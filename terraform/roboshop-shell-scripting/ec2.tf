1b40cabc6ace4c0f90949ecee9b772736f762eab
sivachandu pakalapati <chandumunna066@gmail.com>
2021-07-27 20:53:36 -0400

    Roboshop project scripting

--- terraform/roboshop-shell-scripting/ec2.tf
+++ terraform/roboshop-shell-scripting/ec2.tf
@@ -10,3 +10,9 @@ resource "aws_spot_instance_request" "terraforminstance" {
     Name                 =element(var.COMPONENTS, count.index)
   }
 }
+resource "aws_ec2_tag" "nametag" {
+  count = length(var.COMPONENTS)
+  resource_id = element(aws_spot_instance_request.terraforminstance.*.spot_instance_id, count.index)
+  key         = "Name"
+  value       = element(var.COMPONENTS, count.index )
+}
