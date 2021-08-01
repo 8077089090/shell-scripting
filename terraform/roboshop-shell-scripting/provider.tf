fe671632a871a7a7ba36190fa394d84286a884c2
sivachandu pakalapati <chandumunna066@gmail.com>
2021-08-01 17:07:23 -0400

    Roboshop project using terraform

--- terraform/roboshop-shell-scripting/provider.tf
+++ terraform/roboshop-shell-scripting/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket            = "terraform-b57a"
+    bucket            = "terraform-09"
     key               = "roboshop/shell-scripting/terraform.tfstate"
     region            = "us-east-1"
     dynamodb_table    = "terraform"
